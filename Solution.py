2e18619799c113246b200256d26954a1daaf5a5f
Abdur Rehman Ansari <abdur5148@icloud.com>
2021-11-19 23:30:01 +0530

    Add files via upload

---
+++ Solution.py
@@ -0,0 +1,11 @@
+class Question25:
+    def __init__(self, student, teacher, classfellow):
+        self.student = student
+        self.teacher = teacher
+        self.classfellow = classfellow
+
+
+obj = Question25("Abdur", "Anubhav sir", "Deepak")
+print(obj.student)
+print(obj.teacher)
+print(obj.classfellow)
